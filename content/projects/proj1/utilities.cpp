bc86b3a921d99d3dd596c1ecf7a789ff14450483
travi <travi@LAPTOP-8EUNKVVR>
2019-01-23 11:42:45 -0500

    finished loadData and getNext. Working on saveData

--- content/projects/proj1/utilities.cpp
+++ content/projects/proj1/utilities.cpp
@@ -9,10 +9,14 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <vector>
 
 using namespace std;
 
 process_stats x;
+
+vector <process_stats> p;
 
 int loadData(const char* filename){
 	x = {};
@@ -26,13 +30,41 @@ int loadData(const char* filename){
 	while(infile.good()){
 		string s;
 		getline(infile,s);
+
+		stringstream i(s);
+
+		string z;
+
+		int time = 0;
+		while(getline(i,z,CHAR_TO_SEARCH_FOR)){
+			int y = stoi(z);
+			if(time == 0){
+				x.process_number = y;
+				time++;
+			}else if(time == 1){
+				x.start_time = y;
+				time++;
+			}else{
+				x.cpu_time = y;
+				time = 0;
+			}
+		}
+		p.push_back(x);
+
 	}
 
 	return SUCCESS;
 }
 
 int saveData(const char* filename){
-	return 0;
+	ofstream out;
+
+	out.open(filename);
+	if(!out.is_open()){
+		return COULD_NOT_OPEN_FILE;
+	}
+
+	return SUCCESS;
 }
 
 void sortData(SORT_ORDER mySortOrder){
@@ -40,7 +72,9 @@ void sortData(SORT_ORDER mySortOrder){
 }
 
 process_stats getNext(){
-	return x;
+	process_stats q = p.front();
+	p.pop_back();
+	return q;
 }
 
 
